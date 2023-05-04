a61e891f8d7808d4bebe9aab54d4be69d4c1a92f
João Moura <joao.moura@elysios.com.br>
2023-05-04 03:30:33 -0300

    spec tools setup

--- Gemfile
+++ Gemfile
@@ -11,8 +11,12 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "rspec-rails"
+  gem "factory_bot_rails"
+  gem "faker"
 end
 
-group :development do
+group :test do
+  gem 'shoulda-matchers', '~> 5.0'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_04_062834) do
+  create_table "products", force: :cascade do |t|
+    t.string "name", limit: 100, null: false
+    t.decimal "price", precision: 10, scale: 2, null: false
+    t.string "photo_url", null: false
+    t.integer "status", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
